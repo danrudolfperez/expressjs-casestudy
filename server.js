6652f927c43339ac513874dff0c569afface1a5e
Dan Perez <danrudolf.perez@gmail.com>
2023-01-16 23:50:41 +0800

    test 5

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-// const port = process.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
+// const port = 5000;
 
 app.use(cors());
 app.use(express.json());
