09491ea6985932d68a9435a1b6d91648671fe997
Dan Perez <danrudolf.perez@gmail.com>
2023-01-16 21:34:53 +0800

    update 4

--- server.js
+++ server.js
@@ -37,3 +37,8 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+module.exports = (req, res) => {
+  // res.send("Hello, World!")
+  // res.status(500).json({message: "Something went wrong!"}) 
+  res.json({message: "Hello, World!"})
+}
