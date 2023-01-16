2dd3983e5cf947a8db48f89189b5f30c6d483a52
Dan Perez <danrudolf.perez@gmail.com>
2023-01-16 22:57:26 +0800

    heroku update

--- server.js
+++ server.js
@@ -37,8 +37,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-// module.exports = (req, res) => {
-//   // res.send("Hello, World!")
-//   // res.status(500).json({message: "Something went wrong!"}) 
-//   res.json({message: "Hello, World!"})
-// }
