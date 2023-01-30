2debd82bf9308e62dd5f5fdd8eb32bf43f7258d9
Dan Perez <danrudolf.perez@gmail.com>
2023-01-30 12:51:55 +0800

    sms api update 6

--- routes/smsapi.js
+++ routes/smsapi.js
@@ -2,10 +2,29 @@ const router = require('express').Router();
 const axios = require("axios");
 
 router.route('/').post((req, res) => {
-    // const recipients = req.params.Recipients;
-    // const message = req.params.Message;
-    // const apicode = req.params.Apicode;
-    // const senderid = req.params.SenderId;
+    const recipients = req.params.Recipients;
+    const message = req.params.Message;
+    const apicode = req.params.Apicode;
+    const senderid = req.params.SenderId;
+
+	axios.post("https://api.itexmo.com/api/broadcast", {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
+        },
+        data: {
+            Recipients: [recipients],
+            Message: message,
+            ApiCode: apicode,
+            SenderId: senderid
+        }
+	})
+		.then(response => {
+			res.status(200).json(response.data);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: err });
+		});
 
     // var config = {
     //     method: 'post',
@@ -28,32 +47,32 @@ router.route('/').post((req, res) => {
     //   });
 
 
-    var data = JSON.stringify({
-      "Recipients": "09295043363",
-      "Message": "Test",
-      "ApiCode": "PR-DANRU483889_87FEP",
-      "SenderId": "QUICKLEAN"
-    });
+    // var data = JSON.stringify({
+    //   "Recipients": "09295043363",
+    //   "Message": "Test",
+    //   "ApiCode": "PR-DANRU483889_87FEP",
+    //   "SenderId": "QUICKLEAN"
+    // });
     
-    var config = {
-      method: 'post',
-      url: 'https://cocoon-rewards.com/sms',
-      headers: { 
-        'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ=', 
-        'Content-Type': 'application/json'
-      },
-      data : data
-    };
+    // var config = {
+    //   method: 'post',
+    //   url: 'https://cocoon-rewards.com/sms',
+    //   headers: { 
+    //     'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ=', 
+    //     'Content-Type': 'application/json'
+    //   },
+    //   data : data
+    // };
     
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      res.json(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.json(error);
-    });
+    // axios(config)
+    // .then(function (response) {
+    //   console.log(JSON.stringify(response.data));
+    //   res.json(response.data);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    //   res.json(error);
+    // });
 });
 
 module.exports = router;
