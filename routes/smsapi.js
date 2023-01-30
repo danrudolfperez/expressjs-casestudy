b63d63ab2981f7ee9df1aa24d173ba3e24403bd5
Dan Perez <danrudolf.perez@gmail.com>
2023-01-30 13:21:57 +0800

    sms api update 10

--- routes/smsapi.js
+++ routes/smsapi.js
@@ -1,25 +1,42 @@
 const router = require('express').Router();
 const axios = require("axios");
 
-router.route('/').post((req, res) => {
+router.route('/send').post((req, res) => {
 
     var config = {
-      method: 'post',
-      url: 'https://api.itexmo.com/api/broadcast?Recipients=["09295043363"]&Message=Test&ApiCode=PR-DANRU483889_87FEP&SenderId=QUICKLEAN',
-      headers: { 
-        'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
-      }
-    };
+        method: 'post',
+        url: 'https://api.itexmo.com/api/broadcast?Recipients=["09295043363"]&Message=Test&ApiCode=PR-DANRU483889_87FEP&SenderId=QUICKLEAN',
+        headers: { 
+          'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
+        }
+      };
+      
+      axios(config)
+      .then(function (response) {
+        // console.log(JSON.stringify(response.data));
+        res.status(200).json(response.data);
+      })
+      .catch(function (error) {
+        // console.log(error);
+        res.status(500).json({ message: err });
+      });
+      
     
-    axios(config)
-    .then(function (response) {
-    //   console.log(JSON.stringify(response.data));
-      res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    //   res.status(500).json({ message: err });
-    });
+    // var data = JSON.stringify({
+    //   "Recipients": req.body.Recipients,
+    //   "Message": req.body.Message,
+    //   "ApiCode": req.body.ApiCode,
+    //   "SenderId": req.body.SenderId
+    // });
+
+    // var config = {
+    //   method: 'post',
+    //   url: 'https://api.itexmo.com/api/broadcast',
+    //   headers: { 
+    //     'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
+    //   }
+    // };
+    
 
 
 
