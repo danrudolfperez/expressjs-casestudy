d2b7de6600111d31de8876d5085551fd3832a6b6
Dan Perez <danrudolf.perez@gmail.com>
2023-01-30 12:30:15 +0800

    sms api update 3

--- routes/smsapi.js
+++ routes/smsapi.js
@@ -1,33 +1,61 @@
 const router = require('express').Router();
-const axios = require("axios");
+// const axios = require("axios");
 
-router.route('/add').post((req, res) => {
-    const recipients = req.body.recipients;
-    const message = req.body.message;
-    const apicode = req.body.apicode;
-    const senderid = req.body.senderid;
+router.route('/').post((req, res) => {
+    // const recipients = req.body.recipients;
+    // const message = req.body.message;
+    // const apicode = req.body.apicode;
+    // const senderid = req.body.senderid;
 
+    // var config = {
+    //     method: 'post',
+    //     url: "https://api.itexmo.com/api/broadcast?Recipients=["+recipients+"]&Message="+ message + "&ApiCode="+ apicode + "&SenderId="+senderid+'"',
+    //     headers: { 
+    //       'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
+    //     }
+    //   };
+
+    //   axios(config)
+    //   .then(function (response) {
+    //     // console.log(JSON.stringify(response.data));
+    //     console.log(response.data);
+    //     res.json(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //     res.json(error);
+    //     // console.log(error.data);
+    //   });
+
+    // res.json(results)
+
+    var axios = require('axios');
+    var data = JSON.stringify({
+      "Recipients": "09295043363",
+      "Message": "Test",
+      "ApiCode": "PR-DANRU483889_87FEP",
+      "SenderId": "QUICKLEAN"
+    });
+    
     var config = {
-        method: 'post',
-        url: "https://api.itexmo.com/api/broadcast?Recipients=["+recipients+"]&Message="+ message + "&ApiCode="+ apicode + "&SenderId="+senderid+'"',
-        headers: { 
-          'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
-        }
-      };
-
-      axios(config)
-      .then(function (response) {
-        // console.log(JSON.stringify(response.data));
-        console.log(response.data);
-        res.json(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.json(error);
-        // console.log(error.data);
-      });
-
-    res.json(results)
+      method: 'post',
+      url: 'https://cocoon-rewards.com/sms',
+      headers: { 
+        'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ=', 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.json(error);
+    });
 });
 
 module.exports = router;
