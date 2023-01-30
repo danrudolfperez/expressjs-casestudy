361336291ed7010ef5d9b0b5222fb267f124d8b2
Dan Perez <danrudolf.perez@gmail.com>
2023-01-30 13:02:28 +0800

    sms api update 8

--- routes/smsapi.js
+++ routes/smsapi.js
@@ -2,31 +2,55 @@ const router = require('express').Router();
 const axios = require("axios");
 
 router.route('/').post((req, res) => {
-    const recipients = req.params.Recipients;
-    const message = req.params.Message;
-    const apicode = req.params.Apicode;
-    const senderid = req.params.SenderId;
 
-    var data = JSON.stringify({
-      "Recipients": recipients,
-      "Message": message,
-      "ApiCode": apicode,
-      "SenderId": senderid
+    var config = {
+      method: 'post',
+      url: 'https://api.itexmo.com/api/broadcast?Recipients=["09295043363"]&Message=Test&Apicode=PR-DANRU483889_87FEP&SenderId=QUICKLEAN',
+      headers: { 
+        'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
+      }
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(500).json({ message: err });
     });
 
-	axios.post("https://api.itexmo.com/api/broadcast", {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
-        },
-        data: data
-	})
-		.then(response => {
-			res.status(200).json(response.data);
-		})
-		.catch((err) => {
-			res.status(500).json({ message: err });
-		});
+
+
+
+    // const recipients = req.params.Recipients;
+    // const message = req.params.Message;
+    // const apicode = req.params.Apicode;
+    // const senderid = req.params.SenderId;
+
+    // var data = JSON.stringify({
+    //   "Recipients": recipients,
+    //   "Message": message,
+    //   "ApiCode": apicode,
+    //   "SenderId": senderid
+    // });
+
+	// axios.post("https://api.itexmo.com/api/broadcast", {
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Authorization': 'Basic Y3J5cHRvZGlwdGVyYUBnbWFpbC5jb206SWFtR3JlYXQxMjM0ISQ='
+    //     },
+    //     data: data
+	// })
+	// 	.then(response => {
+	// 		res.status(200).json(response.data);
+	// 	})
+	// 	.catch((err) => {
+	// 		res.status(500).json({ message: err });
+	// 	});
+
+
 
     // var config = {
     //     method: 'post',
@@ -47,6 +71,12 @@ router.route('/').post((req, res) => {
     //     res.json(error);
     //     // console.log(error.data);
     //   });
+
+
+
+
+
+
 
 
     // var data = JSON.stringify({
@@ -75,6 +105,8 @@ router.route('/').post((req, res) => {
     //   console.log(error);
     //   res.json(error);
     // });
+
+
 });
 
 module.exports = router;
